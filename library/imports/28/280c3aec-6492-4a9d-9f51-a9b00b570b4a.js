6e48131021581f72080e787ae42328eeae1b2213
qc88209818 <915618574@qq.com>
2018-10-07 00:23:02 +0800

    1

--- library/imports/28/280c3aec-6492-4a9d-9f51-a9b00b570b4a.js
+++ library/imports/28/280c3aec-6492-4a9d-9f51-a9b00b570b4a.js
@@ -2,23 +2,26 @@
 cc._RF.push(module, '280c3rsZJJKnZ9RqbALVwtK', 'HelloWorld');
 // Script/HelloWorld.js
 
-'use strict';
+"use strict";
 
 cc.Class({
     extends: cc.Component,
 
-    properties: {
-        label: {
-            default: null,
-            type: cc.Label
-        },
-        // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
-    },
+    properties: {},
 
     // use this for initialization
     onLoad: function onLoad() {
-        this.label.string = this.text;
+        // 加载 Prefab
+        cc.loader.loadRes("prefab/btn", function (err, prefab) {
+            for (var i = 0; i < 6; ++i) {
+                for (var j = 0; j < 4; ++j) {
+                    var newNode = cc.instantiate(prefab);
+                    newNode.x = 440 + i * 80;
+                    newNode.y = 320 + j * 80;
+                    cc.director.getScene().addChild(newNode);
+                }
+            }
+        });
     },
 
     // called every frame

b79cee75cb3556d1f6948c15db5196e723fab86c
qc88209818 <915618574@qq.com>
2018-10-19 21:49:58 +0800

    第二版
    1、按钮动态生成
    2、数字随机生成

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -1,20 +1,68 @@
+var NumberButton = cc.Class({
+    extends: cc.Node,
+
+    properties: {
+        index:0,
+        node:cc.Node,
+    },
+
+    init: function (prefab, idx) {
+        this.index = idx;
+        var newNode = cc.instantiate(prefab);
+        newNode.x = 0;
+        newNode.y = 0;
+        this.addChild(newNode);
+
+        this.width = newNode.width;
+        this.height = newNode.height;
+        this.node = newNode;
+        this.setNumber(0);
+    },
+
+    setNumber: function(num) {
+        var labelNode = this.node.getChildByName("label");
+        var labelComp = labelNode.getComponent(cc.Label);
+        labelComp.string = num;
+    },
+
+    setListener: function(func, self) {
+        this.node.on('click', func, self);
+    }
+});
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
-
+        nNanDu: 6,
+        nWidth: 5,
+        nHeight: 5,
+        nodeArr: {
+            default: [],
+            type: cc.Node
+        },
+        numArr: {
+            default: [],
+            type: cc.Integer
+        }
     },
 
     // use this for initialization
     onLoad: function () {
         // 加载 Prefab
+        var self = this;
         cc.loader.loadRes("prefab/btn", function (err, prefab) {
-            for(var i = 0; i < 6; ++i) {
-                for(var j = 0; j < 4; ++j) {
-                    var newNode = cc.instantiate(prefab);
-                    newNode.x = 440 + i * 80;
-                    newNode.y = 320 + j * 80;
+            for(var i = 0; i < self.nWidth; ++i) {
+                for(var j = 0; j < self.nHeight; ++j) {
+                    var newNode = new NumberButton();
                     cc.director.getScene().addChild(newNode);
+                    self.nodeArr.push(newNode);
+
+                    newNode.init(prefab, self.nodeArr.length);
+                    newNode.setPosition(45 + i*85, 45 + j*85);
+                    newNode.setListener(self.onClickButton, self);
+
+                    self.resetNumber();
                 }
             }
         });
@@ -24,4 +72,23 @@ cc.Class({
     update: function (dt) {
 
     },
+
+    resetNumber:function() {
+        for (var i = 0; i < this.nodeArr.length; ++i) {
+            var num = Math.floor(Math.random()*this.nNanDu + 1) ;
+            this.numArr.push(num);
+
+            var node = this.nodeArr[i];
+            node.setNumber(num);
+        }
+    },
+
+    onClickStart: function() {
+        this.resetNumber();
+    },
+
+    onClickButton: function(event) {
+        var btn = event.target;
+        cc.log(btn.index);
+    }
 });
--- temp/quick-scripts/assets/Script/HelloWorld.js
+++ temp/quick-scripts/assets/Script/HelloWorld.js
@@ -4,28 +4,97 @@ cc._RF.push(module, '280c3rsZJJKnZ9RqbALVwtK', 'HelloWorld', __filename);
 
 "use strict";
 
+var NumberButton = cc.Class({
+    extends: cc.Node,
+
+    properties: {
+        index: 0,
+        node: cc.Node
+    },
+
+    init: function init(prefab, idx) {
+        this.index = idx;
+        var newNode = cc.instantiate(prefab);
+        newNode.x = 0;
+        newNode.y = 0;
+        this.addChild(newNode);
+
+        this.width = newNode.width;
+        this.height = newNode.height;
+        this.node = newNode;
+        this.setNumber(0);
+    },
+
+    setNumber: function setNumber(num) {
+        var labelNode = this.node.getChildByName("label");
+        var labelComp = labelNode.getComponent(cc.Label);
+        labelComp.string = num;
+    },
+
+    setListener: function setListener(func, self) {
+        this.node.on('click', func, self);
+    }
+});
+
 cc.Class({
     extends: cc.Component,
 
-    properties: {},
+    properties: {
+        nNanDu: 6,
+        nWidth: 5,
+        nHeight: 5,
+        nodeArr: {
+            default: [],
+            type: cc.Node
+        },
+        numArr: {
+            default: [],
+            type: cc.Integer
+        }
+    },
 
     // use this for initialization
     onLoad: function onLoad() {
         // 加载 Prefab
+        var self = this;
         cc.loader.loadRes("prefab/btn", function (err, prefab) {
-            for (var i = 0; i < 6; ++i) {
-                for (var j = 0; j < 4; ++j) {
-                    var newNode = cc.instantiate(prefab);
-                    newNode.x = 440 + i * 80;
-                    newNode.y = 320 + j * 80;
+            for (var i = 0; i < self.nWidth; ++i) {
+                for (var j = 0; j < self.nHeight; ++j) {
+                    var newNode = new NumberButton();
                     cc.director.getScene().addChild(newNode);
+                    self.nodeArr.push(newNode);
+
+                    newNode.init(prefab, self.nodeArr.length);
+                    newNode.setPosition(45 + i * 85, 45 + j * 85);
+                    newNode.setListener(self.onClickButton, self);
+
+                    self.resetNumber();
                 }
             }
         });
     },
 
     // called every frame
-    update: function update(dt) {}
+    update: function update(dt) {},
+
+    resetNumber: function resetNumber() {
+        for (var i = 0; i < this.nodeArr.length; ++i) {
+            var num = Math.floor(Math.random() * this.nNanDu + 1);
+            this.numArr.push(num);
+
+            var node = this.nodeArr[i];
+            node.setNumber(num);
+        }
+    },
+
+    onClickStart: function onClickStart() {
+        this.resetNumber();
+    },
+
+    onClickButton: function onClickButton(event) {
+        var btn = event.target;
+        cc.log(btn.index);
+    }
 });
 
 cc._RF.pop();
